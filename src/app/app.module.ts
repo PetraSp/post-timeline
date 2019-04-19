8f6d7150b0fe7174b333fdee7d702494a67eb60a
petra <petra.spirkova@apiumhub.com>
2019-04-19 20:42:21 +0200

    employee detail form initialization and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatTableModule,  MatPaginatorModule } from '@angular/material';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {PostComponent} from './components/post/post.component';
-import {TimelineComponent} from './containers/timeline/timeline.component';
-import {EmployeeListComponent} from './containers/employee-list/employee-list.component';
+import { PostComponent } from './components/post/post.component';
+import { TimelineComponent } from './containers/timeline/timeline.component';
+import {EmployeeDetailComponent, EmployeeListComponent} from './containers/employee-list/employee-list.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @NgModule({
+
+  entryComponents: [EmployeeListComponent, EmployeeDetailComponent],
   declarations: [
     AppComponent,
     PostComponent,
     EmployeeListComponent,
-    TimelineComponent
+    TimelineComponent,
+    EmployeeComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
     MatButtonModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatRippleModule,
     MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/containers/employee-list/employee-list.component.ts
+++ src/app/containers/employee-list/employee-list.component.ts
@@ -1,6 +1,15 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
-
+import {Component, Inject, OnInit} from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  FormControl,
+  FormGroupDirective,
+  NgForm
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 export interface Employee {
   id: number;
@@ -10,7 +19,11 @@ export interface Employee {
   name: string;
 }
 
-const ELEMENT_DATA: Employee[] = [
+export interface EditDialogData {
+  employee: Employee;
+}
+
+const employees: Employee[] = [
   {id: 1, username: 'mathilde', phone: 992312312, role: 'CEO', name: 'Mathilde Saylors'},
   {id: 2, username: 'alia', phone: 986733445, role: 'Designer', name: 'Alia Ginder'},
   {id: 3, username: 'freeman', phone: 971232343, role: 'Developer', name: 'Freeman Litten'},
@@ -26,17 +39,80 @@ const ELEMENT_DATA: Employee[] = [
 })
 
 export class EmployeeListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'username', 'phone', 'role', 'name'];
-  dataSource = new MatTableDataSource<Employee>(ELEMENT_DATA);
+  fields: string[] = ['id', 'username', 'phone', 'role', 'name'];
+  employees: Employee[] = employees;
 
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+
   }
 
+  onShowModal(employeeId: 'string'): void {
+    this.dialog.open(EmployeeDetailComponent, {
+      width: '250px',
+      data: {
+        employee: employees.filter(employee => employee.id === Number(employeeId))[0]
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'app-edit-dialog',
+  templateUrl: 'edit-dialog-data.html',
+})
+
+export class EmployeeDetailComponent implements OnInit {
+  public username: AbstractControl;
+  public phone: AbstractControl;
+  public role: AbstractControl;
+  public name: AbstractControl;
+
+  public employeeDetail: FormGroup;
+
+  confirmValidParentMatcher = new ConfirmValidParentMatcher();
+  errors: { [key: string]: string } = {
+    username: 'Username is required and must not contain special characters',
+    phone: 'This phone is invalid',
+    role: 'Role is required',
+    name: 'Name is required',
+  };
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: EditDialogData,  private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.initEmployeeDetailForm();
+  }
+
+  public initEmployeeDetailForm(): void {
+    const { username, phone, role, name } = this.data.employee;
+    this.employeeDetail  =  this.fb.group({
+      username: [
+        username,
+        [
+          Validators.required,
+          Validators.pattern('[\\w-_]+')
+        ]
+      ],
+      phone: [
+        phone,
+        [
+          Validators.required,
+          Validators.pattern('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
+        ]
+      ],
+      role: [role, Validators.required],
+      name: [name, Validators.required],
+    });
+  }
+}
+
+export class ConfirmValidParentMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.invalid && control.touched;
+  }
 }
